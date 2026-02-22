bf4dba212ae9342655c08895ea42aed514e6338a
bhumi110 <bhumisaraogi@gmail.com>
2026-02-22 20:31:51 +0530

    mentors sec commented

--- frontend/src/components/Members.jsx
+++ frontend/src/components/Members.jsx
@@ -51,7 +51,7 @@ const Members = () => {
             </div>
           ))}
         </div>
-        <div className="mentors-section">
+        {/* <div className="mentors-section">
           <h2 className="members-title mentors-heading">
             OUR MENTORS FROM THE REAL WORLD
           </h2>
@@ -72,7 +72,7 @@ const Members = () => {
               </div>
             ))}
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
